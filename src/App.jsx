17002c199090e8fabf97c456b1652d3ec4756d02
vector-10 <duzieblaise10@gmail.com>
2024-02-21 09:43:32 -0500

    chore: setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import './index.css'
+import { Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Awards from "./components/Awards";
 import Contact from "./components/Contact";
-import Skills from './components/Skills';
-
-
 
 
 function App() {
 
   return (
-   <main className="app">   
-    <Hero />    
-   <About />
-   <Skills />
-   <Projects />
-   <Awards />
-   <Contact />
-   </main>
+    <Routes>
+      <Route path='/' element = {<Hero />}  />
+      <Route path='/about' element = {<About />} />
+      <Route path='/projects' element = {<Projects />} />
+      <Route path='/awards' element = {<Awards />} />
+      <Route path='/contact' element = {<Contact />} />
+    </Routes>
+  
   )
 }
 
 export default App
+
