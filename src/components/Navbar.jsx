16c92ddf01b849414eeac0ef7e157170f6384805
vector-10 <duzieblaise10@gmail.com>
2024-02-21 22:50:42 -0500

    chore: UI update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom'; // If using React Router for navigation
 const Sidebar = () => {
   return (
     <div className=" h-screen  bg-gray-900 text-white">
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+      <ul className='flex flex-col items-center pt-10'>
+        <li className='py-3'>Home</li>
+        <li className='py-3'>About</li>
+        <li className='py-3'>Projects</li>
+        <li className='py-3'>Awards</li>
+        <li className='py-3'>Contact</li>        
       </ul>
     </div>
   );
