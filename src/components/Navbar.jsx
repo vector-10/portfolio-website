71a3b9cdb5c02fcfd6f505a3572823d94fa0a450
vector-10 <duzieblaise10@gmail.com>
2024-02-23 13:24:14 -0500

    chore:navbar updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // If using React Router for navigation
 import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import XIcon from '@mui/icons-material/X';
 
 const Sidebar = () => {
   return (
-    <div className=" h-screen bg-gray-900 text-white flex flex-col justify-items-end">
+    <div className=" h-screen bg-gray-900 text-white flex flex-col">
       <ul className='flex flex-col items-center pt-10 text-xl'>
         <li className='py-3 nav-item '><Link to="/">Home</Link></li>
         <li className='py-3 nav-item '><Link to="/about">About</Link></li>
@@ -13,10 +15,10 @@ const Sidebar = () => {
         <li className='py-3 nav-item '><Link to="/contact">Contact</Link></li>        
       </ul>
 
-      <ul className='flex flex-col'>
+      <ul className='flex flex-col items-center mt-5'>
         <li><GitHubIcon /></li>
-        
-
+        <li><LinkedInIcon /></li>
+        <li><XIcon /></li>
       </ul>
     </div>
   );
