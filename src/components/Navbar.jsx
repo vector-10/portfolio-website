ad217ff744c288943bfa9dabfba270f28380918e
VectorWare <97763469+vector-10@users.noreply.github.com>
2024-05-09 19:01:17 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // If using React Router for navigation
+import { Link } from 'react-router-dom'; 
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import XIcon from '@mui/icons-material/X';
