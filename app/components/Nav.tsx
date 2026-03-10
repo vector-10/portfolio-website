78d7c52fceed00891a0917c047a32085f87de78b
vector-10 <duzieblaise10@gmail.com>
2026-03-10 21:52:55 +0100

    chore: typo fix

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-const navLinks = ["About", "Projects", "Experience", "Contact"];
+const navLinks = ["About", "Projects", "Contact"];
 
 function SunIcon() {
   return (
