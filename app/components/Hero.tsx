0c54e616cab9c6f659b16ca8a864e2c170090ce1
vector-10 <duzieblaise10@gmail.com>
2026-04-13 00:09:24 +0100

    chore: few updates

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -76,7 +76,7 @@ export default function Hero() {
                 href="#contact"
                 className="px-6 py-3 rounded-xs border border-wire text-snow text-sm hover:border-mist transition-colors"
               >
-                Let&apos;s Talk
+                Send An Email
               </a>
             </div>
 
